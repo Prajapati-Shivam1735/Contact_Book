cb8660f3229960d799dabb81ef4bec8e14638fa1
Shivam Prajapati <124157712+Prajapati-Shivam1735@users.noreply.github.com>
2025-08-19 19:06:11 +0530

    Update contact_book.sql

--- contact_book.sql
+++ contact_book.sql
@@ -6,10 +6,4 @@ CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     phone VARCHAR(20),
-    email VARCHAR(100)
 );
-
-ALTER TABLE contacts DROP COLUMN email;
-
-SELECT * FROM contacts;
-DESCRIBE contacts
